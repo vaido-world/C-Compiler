6b3c2a2c443824ab75cf2144b445b7ebcc49198a
BoQsc <BoQsc@users.noreply.github.com>
2021-11-11 22:44:25 +0200

    Update lexical_scanner.c

--- Compiler/lexical_analysis/lexical_scanner.c
+++ Compiler/lexical_analysis/lexical_scanner.c
@@ -13,8 +13,11 @@ void main(int argc, char *argv[])
 	FILE *Infile = Infile = fopen(argv[1], "r");
 	if (Infile == NULL) 
 	{
+		fprintf(stderr, "In File: '%s'  \nOn Line %i:  \nError: Unable to Open File named '%s': %s. (errno: 2) \n", __FILE__, __LINE__, argv[1], strerror(errno));
+		printf("Info: '");
 		get_current_path();
-		fprintf(stderr, "In File: '%s'  \nOn Line %i:  \nError: Unable to Open File named '%s': %s.\n", __FILE__, __LINE__, argv[1], strerror(errno));
+		printf("\\%s", argv[1]);
+		printf("'\n");
 		// _[errno=%i]\n errno
 		exit(1);
 	}
