97915c6feb750cead4c0706f9aed477792fa2d2f
BoQsc <BoQsc@users.noreply.github.com>
2021-11-11 22:11:29 +0200

    Update get_current_path.c

--- Compiler/lexical_analysis/get_current_path.c
+++ Compiler/lexical_analysis/get_current_path.c
@@ -14,15 +14,22 @@
 
 #include <windows.h>
 
+int get_current_path(){
+	char cwd[PATH_MAX];
+	if (getcwd(cwd, sizeof(cwd)) != NULL) 
+	{
+		printf("Current working dir: %s\n", cwd);
+	} else {
+		
+		perror("getcwd() error");
+		return 1;
+	}
+	return 0;
+}
+
 int main() {
-	
-   char cwd[PATH_MAX];
-   if (getcwd(cwd, sizeof(cwd)) != NULL) {
-       printf("Current working dir: %s\n", cwd);
-   } else {
-       perror("getcwd() error");
-       return 1;
-   }
+	get_current_path();
+
    
    return 0;
 }
