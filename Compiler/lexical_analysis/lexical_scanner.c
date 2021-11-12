af4b315f41a42b0c08b5838814a982543c97619a
BoQsc <BoQsc@users.noreply.github.com>
2021-11-12 09:37:18 +0200

    Update lexical_scanner.c

--- Compiler/lexical_analysis/lexical_scanner.c
+++ Compiler/lexical_analysis/lexical_scanner.c
@@ -18,13 +18,31 @@ void main(int argc, char *argv[])
 		
 		fprintf(stderr, "Error: Unable to Open File named '%s': %s. (errno: 2) \n", argv[1], strerror(errno));
 		
+		// Relative Path Error Note
+		// If there is no / at the beginning of the input:
+		// If there is no :\ as part of input
+		// If there is ./ at the beginning of the input:
 		
 		printf("Note: Tried to Open File ('");
 		get_current_path();
 		printf("\\%s", argv[1]);
 		printf("')\n");
+		
+		// Absolute Path Error Note
+		// Simply print the input path
+		// Print which path subfolder is inaccesible
 
 		// _[errno=%i]\n errno
+		
 		exit(1);
+	}
+	else {
+		printf("Opened file: ", argv[1]);
+		printf("'");
+		get_current_path();
+		printf("\\%s' ", argv[1]);
+		printf("without errors. ");
+		puts("");
+		
 	}
 }
